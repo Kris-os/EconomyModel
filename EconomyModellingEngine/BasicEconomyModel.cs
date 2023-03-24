a3a92ec42e07a1af06f06403badd4835300dca76
Kristopher OSullivan <kristopher.osullivan@gmail.com>
2023-03-24 18:16:14 +0000

    add components

--- EconomyModellingEngine/BasicEconomyModel.cs
+++ EconomyModellingEngine/BasicEconomyModel.cs
@@ -11,3 +11,21 @@ internal class BasicEconomyModel
         return results;
     }
 }
+
+//Assumption
+//Everyone is self-sufficient, it's the interaction between the price for luxury goods that will be interesting
+//You need the people making decisions based on the market price, and the market price adjusting to the decisions of the people (to produce either Luxury or Necessary)
+
+//Introduce dynamic demand for Luxuries
+//People will seek to consume more luxuries if more are available
+
+//Introduce money
+//People will have money rather than stockpiles of goods (or both!)
+
+//Introduce specialisation
+//People can become better at creating either Luxury or Necessary.  The overall economy grows.
+//Mechanism for the economy to make enough "Necessary" goods?
+//
+//Introduce trading rate
+//Goods can be exchanged at the going rate
+//Individuals will maximise profit - producing goods that can be traded
